489236c4e99df2c974b0b9cd94d16fd0a4c1e3a5
ranjeet-singh45 <singhranjeet1303@gmail.com>
2025-07-05 10:11:56 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,43 @@ import ForgotPassword from "./pages/ForgotPassword.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import NewTicket from "./pages/NewTicket.jsx";
 import MyTicket from "./pages/MyTicket.jsx";
+import ProtectedRoute from "./utils/ProtectedRoute.jsx";
 
 function App() {
   return (
     <Routes>
+      {/* Public routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/forgotpassword" element={<ForgotPassword />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/new-ticket" element={<NewTicket />} />
-      <Route path="/my-ticket" element={<MyTicket />} />
+
+      {/* Protected routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/new-ticket"
+        element={
+          <ProtectedRoute>
+            <NewTicket />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/my-ticket"
+        element={
+          <ProtectedRoute>
+            <MyTicket />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Redirect root path */}
       <Route path="/" element={<Navigate to="/login" replace />} />
     </Routes>
   );
